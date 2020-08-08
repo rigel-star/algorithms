a89eaed3ee59d4c830454698b9590e3edd41443e
Rigel Star <boyzdod@gmail.com>
2020-08-08 23:28:35 +0545

    trig funcs

---
+++ image/trigfuncs.go
@@ -0,0 +1,28 @@
+package main
+
+const (
+	sine = 1
+	cos = 2
+	tan = 3
+)
+
+type wave struct {
+	typ int
+}
+
+func drawWave(w wave) {
+	switch w.typ {
+
+	case sine:
+		break
+	case cos:
+		break
+	case tan:
+		break
+	}
+}
+
+func main() {
+	w := wave{sine}
+	drawWave(w)
+}
