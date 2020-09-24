97dcdecdb0883bebab2825495f8297ed5a8dc071
RigelStar <47748256+rigel-star@users.noreply.github.com>
2020-09-24 20:49:17 +0545

    updated with rgb

--- noise.go
+++ noise.go
@@ -1,7 +1,7 @@
 package main
 
-
 import (
+	"colorica"
 	"fmt"
 	"math"
 	"math/rand"
@@ -11,41 +11,81 @@ import (
 	"os"
 )
 
-/*constant variables*/
 const (
 	noiseW = 128
 	noiseH = 128
 )
 
-/*global varibles*/
 var (
 	noise [noiseW][noiseH]float64
+	savePath string = "C:\\Users\\Ramesh\\Desktop\\moreCloudy.jpg"
 )
 
 
 func main() {
 	generateNoise()
+	//saveImg( generateWoodNoise(), savePath )
 
 	img := image.NewRGBA( image.Rect( 0, 0, noiseW, noiseH ))
+
+	var L float64
+	var torgb colorica.RGB
+	var hsl colorica.HSL
+	var rgb color.RGBA
 
 	for x := 0; x < noiseW; x++ {
 		for y := 0; y < noiseH; y++ {
 
+			L = 192 + turbulence( float64(x), float64(y), 64 ) / 4
+			hsl = colorica.HSL{169.0, 255.0, L} //H = 169, S = 255, L = L
+			torgb = hsl.ToRGB()
+
+			fr := torgb.R * 255.0
+			fg := torgb.G * 255.0
+			fb := torgb.B * 255.0
+
+			//for colored pattern
+			rgb = color.RGBA{R: uint8(fr), G: uint8(fg), B: uint8(fb)}
+
 			//for non smoothed version
-			//rgb := uint8( 256 * noise[x / 8.0][y / 8.0] )
+			//rgb := uint8( noise[x >> 3][y >> 3] << 8 )
 
 			//for smoothed version
-			//rgb := uint8( 256 * smoothNoise( float64(x) / 8.0, float64(y) / 8.0) )
+			//rgb := uint8( smoothNoise( float64(x) / 8.0, float64(y) / 8.0) << 8 )
 
 			//for smoothed and cluody effect
-			rgb := uint8( turbulence( float64(x), float64(y), 256 ))
+			//rgb := uint8( turbulence( float64(x), float64(y), 256 ))
 
-			col := color.RGBA{R: rgb, G: rgb, B: rgb}
-			img.Set( x, y, col )
+			//col := color.RGBA{R: rgb, G: rgb, B: rgb}
+			img.Set( x, y, rgb )
 		}
 	}
 
-	saveImg( img, "C:\\Users\\Ramesh\\Desktop\\moreCloudy.jpg" )
+	saveImg( img, savePath )
+}
+
+
+func generateWoodNoise() image.Image {
+	xyPeriod := 12.0 //number of rings
+  	turbPower := 0.1 //makes twists
+ 	turbSize := 32.0 //initial size of the turbulence
+
+ 	img := image.NewRGBA( image.Rect( 0, 0, noiseW, noiseH ))
+
+ 	for x := 0; x < noiseW; x++ {
+		for y := 0; y < noiseH; y++ {
+
+			xValue := (float64(x) - noiseW / 2.0) / noiseW
+		    yValue := (float64(y) - noiseH / 2.0) / noiseW
+		    distValue := math.Sqrt( xValue * xValue + yValue * yValue ) +
+		    				turbPower * turbulence( float64(x), float64(y), turbSize ) / 256.0
+		    sineValue := 128.0 * math.Abs( math.Sin(2.0 * xyPeriod * distValue * 3.14159 ));
+		    col := color.RGBA{R:uint8( 80 + sineValue ), 
+								G:uint8( 30 + sineValue ), B:30}
+			img.Set( x, y, col )
+		}
+	}
+	return img
 }
 
 
@@ -91,7 +131,7 @@ func smoothNoise( x, y float64 ) float64 {
 
 
 /**
-Generates for cloudy effect.
+Generates cloudy effect.
 */
 func turbulence( x, y, size float64 ) float64 {
 	value := 0.0
@@ -122,11 +162,11 @@ func saveImg( img image.Image, path string ) {
 
 	err = jpeg.Encode( sv, img, nil )
 	if err != nil {
-		fmt.Println("Error generated!")
+		fmt.Println( "Error generated!" )
 	}
 
 	err = sv.Close()
 	if err != nil {
-		fmt.Println("Error generated!")
+		fmt.Println( "Error generated!" )
 	}
 }
